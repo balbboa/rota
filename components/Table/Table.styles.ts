4b835eaf296c358b1fe18c03afcb46b0c65b654d
ARTHUR BALBOA DE MEDEIROS MARTINS <10871262401@PM.GOVRN>
2022-04-18 12:44:46 -0300

    Feat:Add table styles

--- components/Table/Table.styles.ts
+++ components/Table/Table.styles.ts
@@ -1,13 +1,6 @@
 import styled from "styled-components";
 
-type SidebarContainerProps = {
-  isOpened: boolean;
-};
-export const SidebarContainer = styled.aside<SidebarContainerProps>`
-  background-color: ${(props) => props.theme.colors.customBg};
-  width: ${(props) => (props.isOpened ? "20vw" : "0vw")};
-  transition: width 0.5s;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
+export const IconContainer = styled.div`
+  
+
 `;
