b8400a4dcf61439968ab31c26d298718da30ec3d
balbboa <abmm_arthur@hotmail.com>
2022-04-26 11:06:26 -0300

    Feat:branch api

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -4,7 +4,7 @@ import MenuItemsList from "../MenuItemsList";
 
 type SidebarProps = {
   isOpened: boolean;
-};
+}; 
 export default function Sidebar({ isOpened }: SidebarProps) {
   return (
     <SidebarContainer isOpened={isOpened}>
