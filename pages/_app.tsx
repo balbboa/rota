9483a693fe879034d082a5f2bb759c9612fed8df
stephannyb <t.stephanny@bct.ect.ufrn.br>
2022-05-02 10:29:42 -0300

    Feat: Implementa funcionalidade de login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from "next/app";
 import DashboardLayout from "../components/DashboardLayout/DashboardLayout";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
+import SignIn from "./signin";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      {/* <SignIn></SignIn> */}
       <DashboardLayout>
         <Component {...pageProps} />
       </DashboardLayout>
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Container from "../components/Container";
 import React from "react";
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -10,21 +9,39 @@ import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import axios from "axios";
 
+axios.defaults.headers.post['Accept'] = 'application/json'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-const SignIn: NextPage = () => {
+const SignIn: NextPage =  () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+    const obj = {
+      cpf: data.get('cpf'),
+      password: data.get('password')
+    }
+
+
+  axios.get('https://www2.agendamento.pm.rn.gov.br/sispag_ws/v1/public/sanctum/csrf-cookie').then(response => {
+      axios.post(`https://www2.agendamento.pm.rn.gov.br/sispag_ws/v1/public/api/login`, {
+        params: obj, 
+        withCredentials: true
+      }).then(res => {
+        localStorage.setItem('auth_token', res.data.token);
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
     });
+
   };
 
   return (
     <Container title="Sign In">
        <div>
+       <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}"/>
         <section className="signinbg">
         <Box
           sx={{
@@ -42,10 +59,10 @@ const SignIn: NextPage = () => {
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Matrícula"
-              name="email"
-              autoComplete="email"
+              id="cpf"
+              label="CPF"
+              name="cpf"
+              autoComplete="cpf"
               autoFocus
             />
             <TextField
