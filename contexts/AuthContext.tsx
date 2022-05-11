ec18a9c609ff03f33de5186726e0a6976216806d
stephannyb <t.stephanny@bct.ect.ufrn.br>
2022-05-11 12:51:51 -0300

    Feat: adiciona notificacao de erro  no login

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -2,6 +2,9 @@ import {createContext, useState} from "react"
 import axios from 'axios';
 import Router from 'next/router'
 
+import Snackbar from '@mui/material/Snackbar';
+import Alert from "@mui/material/Alert";
+
 
 type SignInData = {
   cpf: string;
@@ -20,8 +23,18 @@ export function AuthProvider({ children }) {
   
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [token, setToken] = useState('')
+  const [open, setOpen] = useState(false);
 
-  async function signIn({cpf, password}: SignInData) {
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+
+  async function signIn({cpf, password}: SignInData){
     try{
       await axios.get('https://www2.agendamento.pm.rn.gov.br/sispag_ws/v1/public/sanctum/csrf-cookie').then(response => {
         axios.post(`https://www2.agendamento.pm.rn.gov.br/sispag_ws/v1/public/api/login`, {cpf, password}).then(res => {
@@ -29,11 +42,14 @@ export function AuthProvider({ children }) {
           setIsAuthenticated(true)
           setToken(res.data.data.token)
           Router.push('/dashboard')
+          
         }).catch(err => {
+          setOpen(true)
           console.log('post error: ', err)
         })
       });        
     }catch(err){
+      setOpen(true)
       console.log(err)
     }
   }
@@ -46,8 +62,16 @@ export function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{isAuthenticated, signIn, signOut}}>
+   <>
+   <AuthContext.Provider value={{isAuthenticated, signIn, signOut}}>
       {children}
     </AuthContext.Provider>
+
+    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} variant="filled" severity="error" sx={{ width: '100%' }}>
+        Usuário ou Senha inválido!
+      </Alert>
+    </Snackbar>
+   </> 
   )
 }
