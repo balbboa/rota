895360687fcd40d5f08b6e683e61099bc1381071
balbboa <abmm_arthur@hotmail.com>
2022-05-11 12:52:46 -0300

    Feat: as

--- components/Routes/index.tsx
+++ components/Routes/index.tsx
@@ -4,12 +4,24 @@ import { AuthContext } from '../../contexts/AuthContext'
 import AuthenticatedLayout from '../AuthenticatedLayout/AuthenticatedLayout'
 import NotAuthenticatedLayout from '../NotAuthenticatedLayout/NotAuthenticatedLayout'
 
-export default function Routes({ Component, pageProps }: AppProps) {
+export default function Routes({ Component, pageProps, ...appProps }: AppProps) {
   const {isAuthenticated} = React.useContext(AuthContext)
 
   const Layout = isAuthenticated ? AuthenticatedLayout : NotAuthenticatedLayout
 
+  // const getContent = () => {
+  //   if ([`/`].includes(appProps.router.pathname))
+  //     return <Component {...pageProps} />;
+
+  //   return (
+  //     <AuthenticatedLayout>
+  //       <Component {...pageProps} />{" "}
+  //     </AuthenticatedLayout>
+  //   );
+  // };
+
   return (
+    // getContent()
     <Layout>
       <Component {...pageProps} />
     </Layout>
