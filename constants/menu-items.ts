9690907079dffd5b5b49b1169c7ac0845d719203
balbboa <abmm_arthur@hotmail.com>
2022-05-19 10:05:48 -0300

    Feat: producao

--- constants/menu-items.ts
+++ constants/menu-items.ts
@@ -1,6 +1,6 @@
 import {
   AccountBalanceWallet, House, InsertInvitation,
-  LocalDining, Person
+  LocalDining
 } from "@styled-icons/material";
 import React from "react";
 
@@ -56,11 +56,11 @@ const MENU_OPTIONS: MenuOption[] = [
     icon: AccountBalanceWallet,
     url: "/diarias",
   },
-  {
-    name: "Marcar DO",
-    icon: Person,
-    url: "/marcacao",
-  },
+  // {
+  //   name: "Marcar DO",
+  //   icon: Person,
+  //   url: "/marcacao",
+  // },
   // {
   //   name: "Sair",
   //   icon: ExitToApp,
