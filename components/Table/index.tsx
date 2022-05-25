8340f8f3d36150fa97198c10ff0506ca84a02fba
root <root@NT007294.pm.govrn>
2022-05-25 11:26:11 -0300

    Feat: organize

--- components/Table/index.tsx
+++ components/Table/index.tsx
@@ -27,8 +27,8 @@ interface Escala {
   local: string,
   prefixo_posto: string,
   inicio: string,
-  termino: string,  
-  situacao: {name:string},
+  termino: string,
+  situacao: { name: string },
   observacao: string
 }
 
@@ -36,8 +36,8 @@ interface Vale {
   titulo_escala: string,
   prefixo_posto: string,
   inicio_posto: string,
-  termino_posto: string,  
-  situacao_vale: {name:string},
+  termino_posto: string,
+  situacao_vale: { name: string },
   observacao: string,
   valor_vale_refeicao: string
 }
@@ -46,7 +46,7 @@ interface Diaria {
   titulo_escala: string,
   data_diaria: string,
   observacao_diaria: string,
-  situacao_diaria: {name:string},
+  situacao_diaria: { name: string },
   valor_diaria: string
 }
 
@@ -58,7 +58,7 @@ interface Marcacao {
   opcoes: string
 }
 
-export default function DataTable({columns, rows}:IParams) {
+export default function DataTable({ columns, rows }: IParams) {
   const [open, setOpen] = useState(false);
   const [escala, setEscala] = useState<Escala>()
   const [vale, setVale] = useState<Vale>()
@@ -89,8 +89,8 @@ export default function DataTable({columns, rows}:IParams) {
     setBtnDisabled(true)
   };
 
-  let string1 = 'eu concordo'  
-  
+  let string1 = 'eu concordo'
+
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <DataGrid
@@ -102,111 +102,111 @@ export default function DataTable({columns, rows}:IParams) {
         getRowId={(row) => row.id}
         rowsPerPageOptions={[10]}
         localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
-        onCellClick ={handleCellOpen}
+        onCellClick={handleCellOpen}
       />
       <Dialog
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">{'Detalhes'}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          { escala?.situacao ? ( 
-            <>
-              <TextModal><span>Título:</span> {escala?.titulo_escala}</TextModal>
-              <TextModal><span>Posto:</span> {escala?.prefixo_posto}</TextModal>
-              <TextModal><span>Local:</span> {escala?.local}</TextModal>
-              <TextModal><span>Início:</span> {escala?.inicio}</TextModal>
-              <TextModal><span>Término:</span> {escala?.termino}</TextModal>
-              <TextModal><span>Situação:</span> {escala?.situacao.name}</TextModal>
-              <TextModal><span>Observação:</span> {escala?.observacao}</TextModal>
-            </>
-          ):('')}
-          { vale?.situacao_vale ? ( 
-            <>
-              <TextModal><span>Título:</span> {vale?.titulo_escala}</TextModal>
-              <TextModal><span>Posto:</span> {vale?.prefixo_posto}</TextModal>
-              <TextModal><span>Início:</span> {vale?.inicio_posto}</TextModal>
-              <TextModal><span>Término:</span> {vale?.termino_posto}</TextModal>
-              <TextModal><span>Valor:</span> {vale?.valor_vale_refeicao}</TextModal>
-              <TextModal><span>Situação:</span> {vale?.situacao_vale.name}</TextModal>
-              <TextModal><span>Observação:</span> {vale?.observacao}</TextModal>
-            </>
-          ):('')}
-          { diaria?.situacao_diaria ? ( 
-            <>
-              <TextModal><span>Título:</span> {diaria?.titulo_escala}</TextModal>
-              <TextModal><span>Data:</span> {diaria?.data_diaria}</TextModal>
-              <TextModal><span>Valor:</span> {diaria?.valor_diaria}</TextModal>
-              <TextModal><span>Situação:</span> {diaria?.situacao_diaria.name}</TextModal>
-              <TextModal><span>Observação:</span> {diaria?.observacao_diaria}</TextModal>
-            </>
-          ):('')}
-          { marcacao?.data_marcacao ? ( 
-            <>
-              <TextModal><span>Código:</span> {marcacao?.codigo}</TextModal>
-              <TextModal><span>Unidade:</span> {marcacao?.unidade}</TextModal>
-              <TextModal><span>Data e Hora:</span> {marcacao?.data_marcacao}</TextModal>
-              <TextModal><span>Quantidade:</span> {marcacao?.quantidade}</TextModal>
-              <TextModal><span>Opções:</span> {marcacao?.opcoes}</TextModal>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleClickOpen}
-              >
-                Marcar
-              </Button>
-              <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-              >
-                <DialogTitle id="alert-dialog-title">
-                  {"Requisitos Obrigatórios para Marcar esta Diária"}
-                </DialogTitle>
-                <DialogContent>
-                  <h4>Termo de Condições e Responsabilidades</h4>
-                  <DialogContentText id="alert-dialog-description"> 
-                    <ul>
-                      <li>A diária operacional é de caráter voluntário, contudo, após confirmar sua voluntariedade para o serviço, o agente ficará na responsabilidade para o cumprimento, podendo este, na sua ausência, trazer prejuízo ao serviço.</li>
-                      <li>O voluntário deverá apresentar-se ao serviço no local e horário informado, com o Uniforme e equipamentos adequados.</li>
-                      <li>É de responsabilidade do voluntário verificar no sistema RotaWeb (menu Minhas Escalas) se não há nenhum choque de horário entre os serviços, para o qual foi escalado, fiscalizado ou voluntário, e informar, o mais breve possível, alterações que o mesmo identificar à administração da sua Unidade.</li>
-                      <li>O voluntário ao marcar uma Diária Opercional no sistema RotaWeb declara estar ciente dos requisitos exigidos para o serviço e que atende os mesmos sem ressalvas.</li>
-                      <li>O voluntário que deixar de participar, a tempo, via SEI, a impossibilidade de comparecer ao serviço, faltar ou chegar atrasado, poderá sobre sanções previstas em lei.</li>
-                      <li>Digite <AgreeSpan>{string1}</AgreeSpan> para prosseguir</li>
-                    </ul>
-                  </DialogContentText>
-                  <TextField
-                    fullWidth
-                    name="termo"
-                    label="Assinatura"
-                    type="text"
-                    onChange={(e) => {
-                      if(e.target.value == string1){ setBtnDisabled(false) } 
-                      else{ setBtnDisabled(true) } 
-                    }}
-                  />
-                </DialogContent>
-                <DialogActions>
-                  <Button disabled={btnDisabled} variant="contained" onClick={handleBClose}>Confirmar</Button>
-                  <Button onClick={handleBClose} autoFocus>
-                    Cancelar
-                  </Button>
-                </DialogActions>
-              </Dialog>
-            </>
-          ):('')}
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} autoFocus>
-          Voltar
-        </Button>
-      </DialogActions>
-    </Dialog>
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Detalhes'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {escala?.situacao ? (
+              <>
+                <TextModal><span>Título:</span> {escala?.titulo_escala}</TextModal>
+                <TextModal><span>Posto:</span> {escala?.prefixo_posto}</TextModal>
+                <TextModal><span>Local:</span> {escala?.local}</TextModal>
+                <TextModal><span>Início:</span> {escala?.inicio}</TextModal>
+                <TextModal><span>Término:</span> {escala?.termino}</TextModal>
+                <TextModal><span>Situação:</span> {escala?.situacao.name}</TextModal>
+                <TextModal><span>Observação:</span> {escala?.observacao}</TextModal>
+              </>
+            ) : ('')}
+            {vale?.situacao_vale ? (
+              <>
+                <TextModal><span>Título:</span> {vale?.titulo_escala}</TextModal>
+                <TextModal><span>Posto:</span> {vale?.prefixo_posto}</TextModal>
+                <TextModal><span>Início:</span> {vale?.inicio_posto}</TextModal>
+                <TextModal><span>Término:</span> {vale?.termino_posto}</TextModal>
+                <TextModal><span>Valor:</span> {vale?.valor_vale_refeicao}</TextModal>
+                <TextModal><span>Situação:</span> {vale?.situacao_vale.name}</TextModal>
+                <TextModal><span>Observação:</span> {vale?.observacao}</TextModal>
+              </>
+            ) : ('')}
+            {diaria?.situacao_diaria ? (
+              <>
+                <TextModal><span>Título:</span> {diaria?.titulo_escala}</TextModal>
+                <TextModal><span>Data:</span> {diaria?.data_diaria}</TextModal>
+                <TextModal><span>Valor:</span> {diaria?.valor_diaria}</TextModal>
+                <TextModal><span>Situação:</span> {diaria?.situacao_diaria.name}</TextModal>
+                <TextModal><span>Observação:</span> {diaria?.observacao_diaria}</TextModal>
+              </>
+            ) : ('')}
+            {marcacao?.data_marcacao ? (
+              <>
+                <TextModal><span>Código:</span> {marcacao?.codigo}</TextModal>
+                <TextModal><span>Unidade:</span> {marcacao?.unidade}</TextModal>
+                <TextModal><span>Data e Hora:</span> {marcacao?.data_marcacao}</TextModal>
+                <TextModal><span>Quantidade:</span> {marcacao?.quantidade}</TextModal>
+                <TextModal><span>Opções:</span> {marcacao?.opcoes}</TextModal>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleClickOpen}
+                >
+                  Marcar
+                </Button>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  <DialogTitle id="alert-dialog-title">
+                    {"Requisitos Obrigatórios para Marcar esta Diária"}
+                  </DialogTitle>
+                  <DialogContent>
+                    <h4>Termo de Condições e Responsabilidades</h4>
+                    <DialogContentText id="alert-dialog-description">
+                      <ul>
+                        <li>A diária operacional é de caráter voluntário, contudo, após confirmar sua voluntariedade para o serviço, o agente ficará na responsabilidade para o cumprimento, podendo este, na sua ausência, trazer prejuízo ao serviço.</li>
+                        <li>O voluntário deverá apresentar-se ao serviço no local e horário informado, com o Uniforme e equipamentos adequados.</li>
+                        <li>É de responsabilidade do voluntário verificar no sistema RotaWeb (menu Minhas Escalas) se não há nenhum choque de horário entre os serviços, para o qual foi escalado, fiscalizado ou voluntário, e informar, o mais breve possível, alterações que o mesmo identificar à administração da sua Unidade.</li>
+                        <li>O voluntário ao marcar uma Diária Opercional no sistema RotaWeb declara estar ciente dos requisitos exigidos para o serviço e que atende os mesmos sem ressalvas.</li>
+                        <li>O voluntário que deixar de participar, a tempo, via SEI, a impossibilidade de comparecer ao serviço, faltar ou chegar atrasado, poderá sobre sanções previstas em lei.</li>
+                        <li>Digite <AgreeSpan>{string1}</AgreeSpan> para prosseguir</li>
+                      </ul>
+                    </DialogContentText>
+                    <TextField
+                      fullWidth
+                      name="termo"
+                      label="Assinatura"
+                      type="text"
+                      onChange={(e) => {
+                        if (e.target.value == string1) { setBtnDisabled(false) }
+                        else { setBtnDisabled(true) }
+                      }}
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button disabled={btnDisabled} variant="contained" onClick={handleBClose}>Confirmar</Button>
+                    <Button onClick={handleBClose} autoFocus>
+                      Cancelar
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </>
+            ) : ('')}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            Voltar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import Container from "../components/Container";
-import React from "react";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
 import axios from 'axios';
 import Image from 'next/image';
+import React from "react";
+import Container from "../components/Container";
+import { BgSection, FormLogin } from "../components/Signin/Signin.styles";
+import { AuthContext } from "../contexts/AuthContext";
 import Rota from '../public/RotaWeb.png';
 
-import { AuthContext } from "../contexts/AuthContext"
-import { BgSection, FormLogin } from "../components/Signin/Signin.styles";
 
 
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = "*"
@@ -23,7 +23,7 @@ axios.defaults.withCredentials = true
 
 function SignIn() {
 
-  const {signIn} = React.useContext(AuthContext)
+  const { signIn } = React.useContext(AuthContext)
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -43,53 +43,53 @@ function SignIn() {
   return (
     <Container title="RotaWeb">
       <BgSection>
-        <input type="hidden"/>
+        <input type="hidden" />
         <section className="signinbg">
-        <FormLogin>
-        <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-          <Image
-          src={Rota}
-          alt="rota"
-          width={240}
-          height={46}
-          />
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="cpf"
-              label="CPF"
-              name="cpf"
-              autoComplete="cpf"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Entrar
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="https://www8.defesasocial.rn.gov.br/autenticador/index.xhtml" variant="body2">
-                  Esqueceu a senha?
-                </Link>
-              </Grid>
-            </Grid>
+          <FormLogin>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Image
+                src={Rota}
+                alt="rota"
+                width={240}
+                height={46}
+              />
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="cpf"
+                  label="CPF"
+                  name="cpf"
+                  autoComplete="cpf"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Senha"
+                  type="password"
+                  id="password"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Entrar
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="https://www8.defesasocial.rn.gov.br/autenticador/index.xhtml" variant="body2">
+                      Esqueceu a senha?
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
             </Box>
-          </Box>
           </FormLogin>
         </section>
       </BgSection>
